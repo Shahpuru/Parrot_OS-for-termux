6e21db9382751b2d5b65e09411a4eb1f6e33bfa2
AKG007 <39687600+akg007@users.noreply.github.com>
2019-05-23 14:17:35 +0530

    Update INSTALLTION.sh

--- INSTALLTION.sh
+++ INSTALLTION.sh
@@ -1,4 +1,4 @@
-apt instalp figlet 
+apt install figlet 
 figlet by akg007
 apt update
 apt upgrade
@@ -10,6 +10,7 @@ figlet updating complete
 
 echo downloading files.....
 apt install git
+cd $HOME
 
 echo installing..........
 pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh
