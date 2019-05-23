86ff42a12521b405985bbaacfc9c84ca6fa4cd4c
AKG007 <39687600+akg007@users.noreply.github.com>
2019-05-23 09:56:28 +0530

    Update INSTALLTION.sh

--- INSTALLTION.sh
+++ INSTALLTION.sh
@@ -10,3 +10,4 @@ echo downloading files.....
 apt install git
 
 
+pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Parrot/parrot.sh && bash parrot.sh
